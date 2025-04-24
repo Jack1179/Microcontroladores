200edcae9599b61db5a1b7a8edff1df337b22df3
Jack1179 <jtascon@unal.edu.co>
2025-04-24 12:38:43 -0500

    Actualizar Laboratorio_1.asm

--- Laboratorios/Lab1/Laboratorio_1.X/Laboratorio_1.asm
+++ Laboratorios/Lab1/Laboratorio_1.X/Laboratorio_1.asm
@@ -3,6 +3,14 @@
 include P18F4550.inc 
    
 ;Segunda seccion configuracion de salidas entradas
+
+CONFIG FOSC=INTOSC_EC
+CONFIG FOSC=EC_EC
+CONFIG FOSC=XT_XT ;cristal es de 3.999Mhz o menos
+CONFIG FOSC =HS ;cristal es 4Mhz o mayoR
+;ACTIVAR SOLO UNO CONFIG DE RELOJ
+
+CONFIG WDT=OFF
     
 ;Tercera seccion de variables y punteros.
 
